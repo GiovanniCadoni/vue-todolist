5883e5a3bc2f595a23f779cdc2c9fb9850da4c69
GiovanniCadoni <giocadoni19@gmail.com>
2023-11-15 22:40:58 +0100

    add: page with javascript

---
+++ js/script.js
@@ -0,0 +1,28 @@
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        return {
+            newTask: {
+                text: "",
+                done: false,
+            },
+            todoList: [],
+        };
+    },
+    methods: {
+        addTask: function() {
+            console.log("copiato");
+            this.todoList.push({
+                text: this.newTask.text,
+                done: this.newTask.done,
+            });
+            //this.todoList.push({...this.newTask})
+            this.newTask.text = "";
+        },
+        deleteTask: function(clickedIndex) {
+            console.log("Elimina");
+            this.todoList.slice(clickedIndex, 1);
+        },
+    },
+}).mount("#app");
